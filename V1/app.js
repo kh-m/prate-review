e61a320502fa198a1d1f117c5da0c387430ee11c
Khaled M <k.code@icloud.com>
2018-12-19 00:15:15 -0500

    added to POST:/campgrounds
    -  created a new.ejs that the POST:/campgrounds renders
    - made var campGrounds global
    - added functionality to adding new campsite with photo

--- V1/app.js
+++ V1/app.js
@@ -5,22 +5,30 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs");
 
+var campGrounds = [
+    {name: "Kamloops", img: "https://s3.amazonaws.com/imagescloud/images/medias/camping/camping-tente.jpg"},
+    {name: "Whistler", img: "http://iluvesports.com/wp-content/uploads/2014/08/camping-outdoors.jpg"},
+    {name: "Squamish", img: "http://s3.amazonaws.com/digitaltrends-uploads-prod/2017/06/camping-tent-1500x1000.png"}
+]
+
 app.get("/", function(req, res){
     res.render("landing");
 })
 
 app.get("/campgrounds", function(req, res){
-    var campGrounds = [
-        {name: "Kamloops", img: "https://s3.amazonaws.com/imagescloud/images/medias/camping/camping-tente.jpg"},
-        {name: "Whistler", img: "http://iluvesports.com/wp-content/uploads/2014/08/camping-outdoors.jpg"},
-        {name: "Squamish", img: "http://s3.amazonaws.com/digitaltrends-uploads-prod/2017/06/camping-tent-1500x1000.png"}
-    ]
-
     res.render("campgrounds", {campGrounds: campGrounds});
 })
 
 app.post("/campgrounds", function(req, res){
-    res.send("you hit the post route.");
+    var name = req.body.name;
+    var img = req.body.img;
+    var newCampGround = {name: name, img: img};
+    campGrounds.push(newCampGround);
+    res.redirect("/campgrounds");
+})
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new");
 })
 
 app.listen(8000, function(){
