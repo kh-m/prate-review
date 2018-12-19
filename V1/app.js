0c4d34b5d99fd839b7b04ad944506199a11a5d73
Khaled M <k.code@icloud.com>
2018-12-18 23:21:13 -0500

    added GET:/campgrounds
    - var campGrounds with 3 objects in it
    - corresponding campgrounds.ejs file
    - <a> in landing page to /campgrounds

--- V1/app.js
+++ V1/app.js
@@ -6,6 +6,16 @@ app.get("/", function(req, res){
     res.render("landing");
 })
 
+app.get("/campgrounds", function(req, res){
+    var campGrounds = [
+        {name: "Kamloops", img: "https://s3.amazonaws.com/imagescloud/images/medias/camping/camping-tente.jpg"},
+        {name: "Whistler", img: "http://iluvesports.com/wp-content/uploads/2014/08/camping-outdoors.jpg"},
+        {name: "Squamish", img: "http://s3.amazonaws.com/digitaltrends-uploads-prod/2017/06/camping-tent-1500x1000.png"}
+    ]
+
+    res.render("campgrounds", {campGrounds: campGrounds});
+})
+
 app.listen(8000, function(){
     console.log("Camp Server");
 });
