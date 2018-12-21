8d16d9306363067a8d5eb2635d78b392d7adede2
Khaled M <k.code@icloud.com>
2018-12-21 00:32:22 -0500

    new var george as Cat + saved to DB

--- data/cats.js
+++ data/cats.js
@@ -11,9 +11,24 @@ var catSchema = new mongoose.Schema({
 // Creats a Cat object that allows us to use methods like Cat.find Cat.remove etc.
 // "Cat" in model is the singular version of the collection name;
 // Mongoose knows how to make it plural ie cat > cats, person > people etc
-var Cat = mongoose.model("Cat, catSchema");
-
+var Cat = mongoose.model("Cat", catSchema);
 
 // add a cat to database
+var george = new Cat({
+    name: "George",
+    age: 11,
+    temperament: "Grouchy"
+});
+
+// callback function added to make sure that the method can take time
+// and this ensures that the code is run after saving is done
+george.save(function (err, cat) {
+    if(err) {
+        console.log("SOMETHING WENT WRONG!")
+    } else {
+        console.log("WE SAVED A CAT TO THE DB");
+        console.log(cat);
+    }
+});
 
 // retrieve cats from database
