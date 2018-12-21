ee2a32210e4ac19f0abdd7e57ff3867f24fd3c13
Khaled M <k.code@icloud.com>
2018-12-21 11:25:26 -0500

    GET:/campgrounds now references the DB + commented out Camp.create (to not create more camps)

--- V2/app.js
+++ V2/app.js
@@ -16,26 +16,33 @@ var campSchema = new mongoose.Schema({
 // Makes a model using above schema with methods in it like Campground.find() etc.
 var Camp = mongoose.model("Camp", campSchema);
 
-Camp.create(
-    {
-        name: "Whistler",
-        img: "http://iluvesports.com/wp-content/uploads/2014/08/camping-outdoors.jpg"
+// Camp.create(
+//     {
+//         name: "Whistler",
+//         img: "http://iluvesports.com/wp-content/uploads/2014/08/camping-outdoors.jpg"
     
-    }, function(err, camp){
-        if(err){
-            console.log(err);
-        } else {
-            console.log("New camp added!");
-            console.log(camp);
-        }
-    });
+//     }, function(err, camp){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log("New camp added!");
+//             console.log(camp);
+//         }
+//     }
+// );
 
 app.get("/", function(req, res){
     res.render("landing");
 })
 
 app.get("/campgrounds", function(req, res){
-    res.render("campgrounds", {campGrounds: campGrounds});
+    Camp.find({}, function(err, allCamps){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("campgrounds", {camps: allCamps})
+        }
+    })
 })
 
 app.post("/campgrounds", function(req, res){
