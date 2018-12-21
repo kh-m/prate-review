cd45f9f93f46c5dd2a12770ddfe96ce6710b169f
Khaled M <k.code@icloud.com>
2018-12-21 00:43:06 -0500

    changed 'george' details to add a new cat, then commented it out to not add new cat each time running "node cats.js"

--- data/cats.js
+++ data/cats.js
@@ -13,22 +13,26 @@ var catSchema = new mongoose.Schema({
 // Mongoose knows how to make it plural ie cat > cats, person > people etc
 var Cat = mongoose.model("Cat", catSchema);
 
-// add a cat to database
-var george = new Cat({
-    name: "George",
-    age: 11,
-    temperament: "Grouchy"
-});
+// // add a cat to database
+// // note that variable name does not matter here, ie if used again but to add other
+// // cats, the george.save method only take in the data inside of it to save to DB, so
+// // george is just how we refer to it insude our code, but not DB
+// var george = new Cat({
+//     name: "Mrs. Norris",
+//     age: 7,
+//     temperament: "Evil"
+// });
 
-// callback function added to make sure that the method can take time
-// and this ensures that the code is run after saving is done
-george.save(function (err, cat) {
-    if(err) {
-        console.log("SOMETHING WENT WRONG!")
-    } else {
-        console.log("WE SAVED A CAT TO THE DB");
-        console.log(cat);
-    }
-});
+// // callback function added to make sure that the method can take time
+// // and this ensures that the code is run after saving is done
+// // Note: cat in function is what is being sent back from db
+// george.save(function (err, cat) {
+//     if(err) {
+//         console.log("SOMETHING WENT WRONG!")
+//     } else {
+//         console.log("WE SAVED A CAT TO THE DB");
+//         console.log(cat);
+//     }
+// });
 
 // retrieve cats from database
